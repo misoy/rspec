b385fd2765b57174046135f2dee6ad95c9bf6122
Nina Michelle Magpuyo <michellemagpuyo@gmail.com>
2021-04-07 16:22:38 +0800

    Implement described class

---
+++ spec/described_class_spec.rb
@@ -0,0 +1,21 @@
+class Royal 
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+RSpec.describe Royal do
+  subject {
+    described_class.new('Boris')
+  }
+
+  let(:louis) {
+    described_class.new('Louis')
+  }
+
+  it 'represents a great person' do
+    expect(subject.name).to eq('Boris')
+  end
+end
