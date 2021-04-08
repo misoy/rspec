32857fc662ae746a1d860dc8537f317dcbf53979
Nina Michelle Magpuyo <michellemagpuyo@gmail.com>
2021-04-08 12:24:42 +0800

    Implement predicate matchers

---
+++ spec/predicate_methods_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe 'predicate methods and predicate matchers' do
+  it 'can be tested with Ruby methods' do
+    result = 16 / 2
+    expect(result.even?).to eq(true)
+  end
+
+  it 'can be tested with predicate matchers' do
+    result = 16 / 2
+    expect(result).to be_even
+    expect(15).to be_odd
+    expect(0).to be_zero
+    expect([]).to be_empty
+  end
+
+  describe 0 do
+    it { is_expected.to be_zero }
+  end
+end
